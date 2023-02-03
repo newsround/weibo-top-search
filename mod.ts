88f4fce1c42a905a806f012118424e3e73d0138c
SeiriRyu <56245152+RyuSeiri@users.noreply.github.com>
2023-02-03 17:08:27 +0900

    add comment

--- mod.ts
+++ mod.ts
@@ -3,7 +3,6 @@
 import { format } from "std/datetime/mod.ts";
 import { join } from "std/path/mod.ts";
 import { exists } from "std/fs/mod.ts";
-
 import type { Word } from "./types.ts";
 import { createArchive, createReadme, mergeWords } from "./utils.ts";
 
@@ -20,33 +19,31 @@ if (!response.ok) {
   console.error(response.statusText);
   Deno.exit(-1);
 }
-
+//search top weibo result
 const result: string = await response.text();
-
+// content match result
 const matches = result.matchAll(regexp);
-
+// article data {url: url, title:title }
 const words: Word[] = Array.from(matches).map((x) => ({
   url: x[1],
   title: x[2],
 }));
-
+// date format
 const yyyyMMdd = format(new Date(), "yyyy-MM-dd");
+//json path
 const fullPath = join("raw", `${yyyyMMdd}.json`);
-
+//origin data
 let wordsAlreadyDownload: Word[] = [];
 if (await exists(fullPath)) {
   const content = await Deno.readTextFile(fullPath);
   wordsAlreadyDownload = JSON.parse(content);
 }
-
 // save origin data
 const queswordsAll = mergeWords(words, wordsAlreadyDownload);
 await Deno.writeTextFile(fullPath, JSON.stringify(queswordsAll));
-
 // update README.md
 const readme = await createReadme(queswordsAll);
 await Deno.writeTextFile("./README.md", readme);
-
 // update archives
 const archiveText = createArchive(queswordsAll, yyyyMMdd);
 const archivePath = join("archives", `${yyyyMMdd}.md`);
